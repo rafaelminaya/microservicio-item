dfc4cb1aaa3dad55833529c7bcfd05c1d90a61bc
Rafael Minaya <rafael_mr20@outlook.com>
2024-01-06 20:29:13 -0500

    Agregar la dependencia y configuracion a este microservicio como un eureka client

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,22 +2,21 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /*
- * @RibbonClient
- * Configura el/los "Ribbon Client" que tengamos.
- * En este caso a la clase "ProductoClienteRest" anotada con "@FeignClient"
- * Debemos usar "@RibbonClients" en caso tuviesemos mas "Ribbon Client".
- * 
+ * @EnableEurekaClient
+ * Indica que el proyecto actual es un "cliente" que será registrado en un "eureka server".
+ * Ribbon ya viene implícito en Eureka, por lo que será removido tanto su anotación como su dependencia de la clase main y del pom.xml. 
+ *
  * name = "servicio-productos : Especifica el microservicio al que se conectará
  * 
  * @EnableFeignClients
  * Habilita/permite el uso los "Feign client" que tengamos implementados en nuestro proyecto.
  * Además que permite inyectar nuestros "Feign client" en diversos componentes de spring(controllers, services, etc.)
  */
-@RibbonClient(name = "servicio-productos")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
