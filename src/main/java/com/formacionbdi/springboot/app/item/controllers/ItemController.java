5344131c469c03d7b48d2a315df160601e1cdf1a
Rafael Minaya <rafael_mr20@outlook.com>
2024-03-12 00:02:38 -0500

    configuracion del microservicio items como cliente del servidor de configuraciones, ademas de agregar la dependencia de spring actuator

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,14 +1,21 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,16 +28,31 @@ import com.formacionbdi.springboot.app.item.models.service.ItemService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
-
+/*
+ * @RefreshScope
+ * Anotación que permite actualizar los componentes del contenedor de spring en tiempo real, sin reiniciar la aplicación.
+ * Es decir, aquellas clases anotadas con @Component y sus derivados (@Service, @RestController, etc).
+ * Esto también aplica a los atributos inyectados con @Value y @Autowired.
+ * Este proecedimiento se realizará mediante un endpoint de Spring Actuator
+ * Requiere instalar esta dependencia en el "pom.xml"
+ */
+@RefreshScope
 @RestController
 public class ItemController {
 	
 	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
 	
+	@Autowired
+	private Environment env;
+	
 	// Inyectamos esta dependencia, con el fin de a implementar el patrón "circuit breaker" usando anotaciones.
 	// El cual es una alternativa programática.
 	@Autowired
 	private CircuitBreakerFactory circuitBreakerFactory;
+	
+	// Esta propiedad se encuentra en el Repositorio Git del "servidor de configuraciones"
+	@Value(value = "${configuracion.texto}")
+	private String texto;
 
 	@Autowired
 	@Qualifier("serviceFeign")
@@ -163,6 +185,29 @@ public class ItemController {
 	
 				
 		return CompletableFuture.supplyAsync(() -> item);
+	}
+	
+	// Endpoint que imprime las propiedades configuaradas en nuestro "servidor de configuraciones"
+	// El argumento corresponde a una "inyección de dependencias" por argumento en un método.
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(
+			@Value(value = "${server.port}") String puerto) {
+		
+		logger.info("Config server - Texto: " + this.texto);
+		logger.info("Config server - Puerto: " + puerto);
+		
+		Map<String, String> json = new HashMap<>();		
+		json.put("texto", this.texto);
+		json.put("puerto", puerto);
+		
+		// Validación de que existe algún "profile" y que este sea igual a "dev"
+		if(env.getActiveProfiles().length > 0 && this.env.getActiveProfiles()[0].equals("dev")) {
+			// Agregamos los las propiedades del profile "dev" al Hash
+			json.put("autor.nombre", this.env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", this.env.getProperty("configuracion.autor.email"));
+		}				
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 	
 
